37744805f350ea2585d75ad1fd9e22379e5efc6b
Roman Eidenzon <romandmitri@gmail.com>
2026-08-17 10:52:43 -0400

    Adding [DEV_ALPHA] env.

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 This is my personal website hosted on [romandmitri.com](https://romandmitri.com) via Vercel.
 
 If you are here, you are probably curious if I am a good hire... and I am!
+
+--- 
+
+# Repository Setup
+
+## Secrets
+
+Create `/.env` file with the following keys, as needed:
+
+```
+DEV_ALPHA=bravo!
+```
+
+## Run
+
+```
+npm run dev
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
+import { Config } from "@/src/app/common/Config";
 import romanDmitriLogo from "@/src/app/images/romandmitri-logo-square-64x64.png";
 import Image from "next/image";
 
 export default function Home() {
-	const version = process.env.APP_VERSION;
 	return (
 		<div className={"p-2 font-mono"}>
 			<div className={"flex flex-row items-center gap-2"}>
 				{false && <Image src={romanDmitriLogo} alt={"logo"} className={"size-8"} />}
 				<div>
-					<h1 className={"text-developer text-xl"}>
-						{"romandmitri.com"} <small className={"text-developer text-xs"}>{version}</small>
+					<h1 className={"text-developer text-3xl"}>
+						{"romandmitri.com"} <small className={"text-developer text-xs"}>{Config.Version}</small>
 					</h1>
 				</div>
 			</div>
 			<div className={""}>
-				<p className={"text-muted-foreground text-sm"}>{"20260817 Working on something special for you..."}</p>
+				<p className={""}>{"20260817 Working on something special for you..."}</p>
 			</div>
+			<pre className={"text-xs"}>{JSON.stringify({ Config }, null, "\t")}</pre>
 		</div>
 	);
 }
