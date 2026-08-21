3c654421aa9a8a3a5f193e7caab16dfcd9de6534
Roman Eidenzon <romandmitri@gmail.com>
2026-08-21 10:37:10 -0400

    Eh... revert [data-is-open] attribute. Was weird on mobile and too complicated to flag. Should load via widget settings instead.

---
+++ src/common/components/shadcn/mobile.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// TODO: reidenzon - Consider alternatives:
+//  https://www.usehooks.io/docs/use-is-mobile
+//  https://tailwindcss.com/docs/responsive-design
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+	const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
+
+	useEffect(() => {
+		const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+		const onChange = () => {
+			setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+		};
+		mql.addEventListener("change", onChange);
+		setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+		return () => mql.removeEventListener("change", onChange);
+	}, []);
+
+	return !!isMobile;
+}
